3ae2b09ba5529c8fa1ea335e3e0dedccf5cac52b
joelDiéguez <31885931+jdieguvi15@users.noreply.github.com>
2025-06-03 16:38:43 +0200

    added semantics from file

--- Model/model/semantic.py
+++ Model/model/semantic.py
@@ -50,6 +50,7 @@ def generate_descriptions(args):
     elif args['semantics_from'] == 'file':
             with open('../' + args['file'], 'r') as f:
                 class_descriptions = json.load(f)
+                print("hola")
                 print(class_descriptions)
                 return class_descriptions
 
